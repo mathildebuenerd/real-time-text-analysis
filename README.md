57f9ffef2dbea8535a6760447eadd89d086018a2
mathildebuenerd <mathilde.buenerd@gmail.com>
2019-04-29 14:55:13 +0200

    init the typescript + parcel project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real-time sentiment analysis
+_Tool for doing an AFINN sentiment analysis on user input in real time._
---
+++ ts/WritingApp.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+export class WritingApp {
+
+    constructor() {
+
+    }
+
+    init() {
+        console.log(`j'init`);
+    }
+
+}
+
+const app = new WritingApp();
+app.init();
